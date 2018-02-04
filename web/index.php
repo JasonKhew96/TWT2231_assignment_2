6834a4c99740a25bc51dc605c45f1847e38fc472
JasonKhew96 <29531167+JasonKhew96@users.noreply.github.com>
2018-02-04 16:35:49 +0800

    Huge update

--- web/index.php
+++ web/index.php
@@ -1,141 +1,149 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
-require_once __DIR__.'/../vendor/autoload.php'; // Add the autoloading mechanism of Composer
+// use Symfony\Component\HttpFoundation\Response;
 
-$app = new Silex\Application(); // Create the Silex application, in which all configuration is going to go
+require_once __DIR__.'/../vendor/autoload.php';
 
-
-
-// Section A
-// We will later add the configuration, etc. here
+$app = new Silex\Application();
 
 $app['debug'] = true;
-$app['articles'] = array(
-    array(
-        'title'    => 'Lorem ipsum dolor sit amet',
-        'contents' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis vestibulum ultricies. Sed sit amet sagittis nisl. Nulla leo metus, efficitur non risus ut, tempus convallis sem. Mauris pharetra sagittis ligula pharetra accumsan. Cras auctor porta enim, a eleifend enim volutpat vel. Nam volutpat maximus luctus. Phasellus interdum elementum nulla, nec mollis justo imperdiet ac. Duis arcu dolor, ultrices eu libero a, luctus sollicitudin diam. Phasellus finibus dictum turpis, nec tincidunt lacus ullamcorper et. Praesent laoreet odio lacus, nec lobortis est ultrices in. Etiam facilisis elementum lorem ut blandit. Nunc faucibus rutrum nulla quis convallis. Fusce molestie odio eu mauris molestie, a tempus lorem volutpat. Sed eu lacus eu velit tincidunt sodales nec et felis. Nullam velit ex, pharetra non lorem in, fringilla tristique dolor. Mauris vel erat nibh.',
-        'author'   => 'Sammy',
-        'date'     => '2014-12-18',
-    ),
-    array(
-        'title'    => 'Duis ornare',
-        'contents' => 'Duis ornare, odio sit amet euismod vulputate, purus dui fringilla neque, quis eleifend purus felis ut odio. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque bibendum pretium ante, eu aliquet dolor feugiat et. Pellentesque laoreet est lectus, vitae vulputate libero sollicitudin consequat. Vivamus finibus interdum egestas. Nam sagittis vulputate lacus, non condimentum sapien lobortis a. Sed ligula ante, ultrices ut ullamcorper nec, facilisis ac mi. Nam in vehicula justo. In hac habitasse platea dictumst. Duis accumsan pellentesque turpis, nec eleifend ex suscipit commodo.',
-        'author'   => 'Sammy',
-        'date'     => '2014-11-08',
-    ),
-);
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-    array(
-     'pdo.server' => array(
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
         'driver'   => 'pgsql',
         'user' => $dbopts["user"],
         'password' => $dbopts["pass"],
         'host' => $dbopts["host"],
         'port' => $dbopts["port"],
-        'dbname' => ltrim($dbopts["path"],'/')
-        )
-    )
-);
-
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-       'driver'   => 'pgsql',
-       'user' => $dbopts["user"],
-       'password' => $dbopts["pass"],
-       'host' => $dbopts["host"],
-       'port' => $dbopts["port"],
-       'dbname' => ltrim($dbopts["path"],'/')
+        'dbname' => ltrim($dbopts["path"], '/')
        )
 ));
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views', // The path to the templates, which is in our case points to /var/www/templates
+// $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+//     'db.options' => array(
+//        'driver'   => 'pdo_pgsql',
+//        'user' => 'postgres',
+//        'password' => 'toor',
+//        'host' => 'localhost',
+//        'port' => 5432,
+//        'dbname' => 'staff_payroll'
+//        )
+// ));
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'login_path' => array(
+            'pattern' => '^/login$',
+            'anonymous' => true
+        ),
+        'default' => array(
+            'pattern' => '^/.*$',
+            'anonymous' => true,
+            'form' => array(
+                'login_path' => '/login',
+                'check_path' => 'login_check'
+            ),
+            'logout' => array(
+                'logout_path' => '/logout',
+                'invalidate_session' => false
+            ),
+            'users' => function () use ($app) {
+                return new App\User\UserProvider($app['db']);
+            },
+        )
+    ),
+    'security.access_rules' => array(
+        array('^/login$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
+        array('^/.+$', 'IS_AUTHENTICATED_FULLY'),
+    )
 ));
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 
+// function
 
-
-
-$app->get('/', function (Silex\Application $app)  { // Match the root route (/) and supply the application as argument
-    return $app['twig']->render( // Render the page index.html.twig
+$app->get('/', function (Silex\Application $app) {
+    return $app['twig']->render(
         'index.html.twig',
         array(
-            'articles' => $app['articles'], // Supply arguments to be used in the template
+            'title' => 'Home'
         )
     );
 })->bind('index');
 
-$app->get('/info', function (Silex\Application $app)  { // Match the root route (/) and supply the application as argument
+$app->get('/info', function (Silex\Application $app) {
     return phpinfo();
 });
 
-$app->get('/login', function (Silex\Application $app)  { // Match the root route (/) and supply the application as argument
-    return $app['twig']->render( // Render the page index.html.twig
-        'login.html.twig'
+$app->get('/login', function (Silex\Application $app, Request $request) {
+    return $app['twig']->render(
+        'login.html.twig',
+        array(
+            'title' => 'Login',
+            'error' => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
+        )
     );
 })->bind('login');
 
-$app->post('/actionlogin', function (Request $request) {
-    $username = $request->get('username');
-    if ($username == null) {
-        $app->abort(400, 'Username is empty.');
-    }
-    $password = $request->get('password');
-    if ($password == null) {
-        $app->abort(400, 'Password is empty.');
-    }
-    $type = $request->get('type');
-    if ($type == null) {
-        $app->abort(400, 'Type is missing.');
-    }
-    return new Response("$username $password $type", 200);
-})->bind('actionlogin');
-
-$app->get('/stafflist', function () use($app) {
-    $st = $app['pdo']->prepare('SELECT * FROM staff');
-    $st->execute();
-
-    $stafflists = array();
-    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-        $stafflists[] = $row;
-    }
-
+$app->get('/stafflist', function () use ($app) {
+    $stafflists = $app['db']->fetchAll('SELECT * FROM staff');
     return $app['twig']->render('stafflist.html.twig', array(
+        'title' => 'Staff List',
         'stafflists' => $stafflists
     ));
-
-    // $stafflists = $app['db']->fetchAll('SELECT * FROM table');
-    // return $app['twig']->render('stafflist.html.twig', array(
-    //     'stafflists' => $stafflists
-    // ));
-
-    // $dbconn3 = pg_connect("host=localhost port=5432 dbname=staff_payroll user=postgres password=toor");
-
-    // return "test";
 })->bind('stafflist');
 
-$app->get('/{id}', function (Silex\Application $app, $id)  { // Add a parameter for an ID in the route, and it will be supplied as argument in the function
-    if (!array_key_exists($id, $app['articles'])) {
-        $app->abort(404, 'The article could not be found');
+$app->get('/staff/{id}', function (Silex\Application $app, $id) {
+    $profile = $app['db']->fetchAssoc("SELECT * FROM staff, account, department where staff.staff_id = '$id' AND staff.staff_id = account.staff_id AND department.depart_id = staff.depart_id");
+    if (!$profile) {
+        $app->abort(404, "Staff $id could not be found");
     }
-    $article = $app['articles'][$id];
-    return $app['twig']->render(
-        'single.html.twig',
-        array(
-            'article' => $article,
-        )
-    );
+    return $app['twig']->render('staff.html.twig', array(
+        'title' => "Staff $id",
+        'profile' => $profile
+    ));
 })
-    ->assert('id', '\d+') // specify that the ID should be an integer
-    ->bind('single'); // name the route so it can be referred to later in the section 'Generating routes'
+    ->assert('id', '\d+')
+    ->bind('staff');
+
+$app->get('/staffpayroll/{id}', function (Silex\Application $app, $id) {
+    $payrolls = $app['db']->fetchAll("SELECT * FROM staff, account, payroll, department where staff.staff_id = '$id' AND staff.staff_id = account.staff_id AND staff.staff_id = payroll.staff_id AND department.depart_id = staff.depart_id");
+    if (!$payrolls) {
+        $app->abort(404, "Staff $id could not be found");
+    }
+    return $app['twig']->render('staffpayroll.html.twig', array(
+            'title' => "Staff $id",
+            'payrolls' => $payrolls
+        ));
+})
+        ->assert('id', '\d+')
+        ->bind('staffpayroll');
+
+$app->get('/profile', function () use ($app) {
+    $usr = $app['security.token_storage']->getToken()->getUser();
+    $id = $usr->getUsername();
+    $profile = $app['db']->fetchAssoc("SELECT * FROM staff, account, department where username = '$id' AND staff.staff_id = account.staff_id AND department.depart_id = staff.depart_id");
+    return $app['twig']->render('profile.html.twig', array(
+        'title' => 'Profile',
+        'profile' => $profile
+    ));
+})->bind('profile');
+
+$app->get('/payroll', function () use ($app) {
+    $usr = $app['security.token_storage']->getToken()->getUser();
+    $id = $usr->getUsername();
+    $payrolls = $app['db']->fetchAll("SELECT * FROM staff, account, payroll, department where username = '$id' AND staff.staff_id = account.staff_id AND payroll.staff_id = staff.staff_id AND department.depart_id = staff.depart_id");
+    return $app['twig']->render('payroll.html.twig', array(
+        'title' => 'Payroll',
+        'payrolls' => $payrolls
+    ));
+})->bind('payroll');
 
 
-
-
-// This should be the last line
-$app->run(); // Start the application, i.e. handle the request
-?>
+$app->run();
